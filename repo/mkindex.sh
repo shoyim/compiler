1702e5c5692066fa52e829258d7552af9f7178ff
shoyimobloqulov <obloqulovshoyim1@gmail.com>
2026-03-18 05:10:46 +0500

    fixed

--- repo/mkindex.sh
+++ repo/mkindex.sh
@@ -3,12 +3,11 @@
 BASEURL="https://github.com/shoyim/compiler/releases/download/pkgs/"
 i=0
 
-echo -n "" > index
+echo -n "name,version,sha256,url" > index
 
-for pkg in $(find ../packages -type f -name "*.pkg.tar.gz")
-do
-    DIR_NAME=$(basename $(dirname $(dirname $pkg)))
-    VER_NAME=$(basename $(dirname $pkg))
+find ../packages -type f -name "*.pkg.tar.gz" | while read -r pkg; do
+    VER_NAME=$(basename $(dirname "$pkg"))
+    DIR_NAME=$(basename $(dirname $(dirname "$pkg")))
     
     NEW_PKGFILE="${DIR_NAME}-${VER_NAME}.pkg.tar.gz"
     
@@ -16,10 +15,15 @@ do
 
     PKGCHECKSUM=$(sha256sum "./$NEW_PKGFILE" | awk '{print $1}')
 
-    echo "$DIR_NAME,$VER_NAME,$PKGCHECKSUM,$BASEURL$NEW_PKGFILE" >> index
+    echo -e "\n$DIR_NAME,$VER_NAME,$PKGCHECKSUM,$BASEURL$NEW_PKGFILE" >> index
     echo "Adding package $DIR_NAME-$VER_NAME"
     
     ((i=i+1))
 done
+
+if [ ! -s index ] || [ $(cat index | wc -l) -le 1 ]; then
+    echo "Error: No packages found!"
+    exit 1
+fi
 
 echo "Done: $i packages."
