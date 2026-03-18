77b74f9f7db08ed1092371e4705e25f31eaee1c3
shoyimobloqulov <obloqulovshoyim1@gmail.com>
2026-03-18 05:00:21 +0500

    compiler fixed

--- repo/entrypoint.sh
+++ repo/entrypoint.sh
@@ -52,7 +52,6 @@ echo "Index created"
 
 if [[ $SERVER -eq 1 ]]; then
     echo "Starting index server.."
-    # We want the child process to replace the shell to handle signals
     exec python3 /serve.py
 else
     echo "Skipping starting index server"
