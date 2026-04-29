6b58dfa1122d49466e839d476f97bb7ec692580a
shoyimobloqulov <obloqulovshoyim1@gmail.com>
2026-04-29 10:23:16 +0500

    fix(pypy): extract tarball directly to PREFIX, prefer binary wheels

--- packages/pypy/7.3.12/build.sh
+++ packages/pypy/7.3.12/build.sh
@@ -2,17 +2,9 @@
 
 PREFIX=$(realpath $(dirname $0))
 
-mkdir -p build
-cd build
+curl -L "https://downloads.python.org/pypy/pypy3.10-v7.3.12-linux64.tar.bz2" -o "$PREFIX/pypy.tar.bz2"
+tar xjf "$PREFIX/pypy.tar.bz2" --strip-components=1 -C "$PREFIX"
+rm "$PREFIX/pypy.tar.bz2"
 
-curl -L "https://downloads.python.org/pypy/pypy3.10-v7.3.12-linux64.tar.bz2" -o pypy.tar.bz2
-tar xjf pypy.tar.bz2 --strip-components=1
-rm pypy.tar.bz2
-
-cp -r ./* "$PREFIX/"
-
-cd ..
-rm -rf build
-
-bin/pypy3 -m ensurepip
-bin/pypy3 -m pip install numpy scipy pandas pycryptodome sympy
+"$PREFIX/bin/pypy3" -m ensurepip
+"$PREFIX/bin/pypy3" -m pip install --prefer-binary numpy scipy pandas pycryptodome sympy || true
